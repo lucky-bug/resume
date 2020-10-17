1a426336539cf470143a15e2eb9796de31c69998
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2020-10-18 02:42:41 +0700

    Updated links

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 async function fetchData() {
     return {
-        'welcome': await (await fetch('../data/welcome.json')).json(),
-        'about': await (await fetch('../data/about.json')).json(),
-        'educations': await (await fetch('../data/educations.json')).json(),
-        'experiences': await (await fetch('../data/experiences.json')).json(),
-        'languages': await (await fetch('../data/languages.json')).json(),
-        'skills': await (await fetch('../data/skills.json')).json(),
-        'projects': await (await fetch('../data/projects.json')).json(),
-        'awards': await (await fetch('../data/awards.json')).json(),
+        'welcome': await (await fetch('data/welcome.json')).json(),
+        'about': await (await fetch('data/about.json')).json(),
+        'educations': await (await fetch('data/educations.json')).json(),
+        'experiences': await (await fetch('data/experiences.json')).json(),
+        'languages': await (await fetch('data/languages.json')).json(),
+        'skills': await (await fetch('data/skills.json')).json(),
+        'projects': await (await fetch('data/projects.json')).json(),
+        'awards': await (await fetch('data/awards.json')).json(),
     };
 }
 
